58acb5608985b52d9c6fb0615d827495f5613797
João Vitor <vitorjoao39207@gmail.com>
2022-04-17 03:53:15 -0300

    SliderShow pronto, Animações prontas

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 Lista de Tarefas 
 
-( ) - Adicionar conteudos do SliderShow <br>
+(x) - Adicionar conteudos do SliderShow <br>
 (x) - Section Quem Somos <br>
 (x) - Section Serviços <br>
 (x) - Section Contato <br>
 (x) - Adicionar scroll Suave <br>
-( ) - Adicionar Animações
-( ) - Verificador de Formulario
-( ) - Responsividade 
+(X) - Adicionar Animações<br>
+( ) - Verificador de Formulario<br>
+( ) - Responsividade <br>
 ( ) - Hospedar na internet
--- assets/js/script.js
+++ assets/js/script.js
@@ -56,3 +56,18 @@ if(target.length){
         animacaoScroll();
     })
 }
+
+//Animação sites e e-commerece
+
+const titulo = document.querySelector('#h1Sites');
+const texto = document.querySelector('#pSites');
+
+function sites(){
+    titulo.textContent = "Desenvolvimento de Web Sites";
+    texto.innerHTML = "<p>Em tempos remotos um web site realmente era considerado uma página na web, sua estrutura era extremamente simples, com poucos ou nenhum conceito de desing e sem planejamento voltados para acessibilidade e usabilidade. <br/><br/>Não é mais possível dizer que um web site são apenas páginas na interntet, hoje um site deve ser enxergados como um sistema web, que pode ter níveis varíados de desenvolvimento. Hoje é necessario utilizar conhecimento de áreas diferentes para formar um projeto completo.</p>"
+}
+
+function ecommerce() {
+    titulo.textContent = "Desenvolvimento de e-Commerce";
+    texto.innerHTML = "<p>O e-commerce funciona como uma loja virtual e representa um excelente canal de venda online para as empresas. Na prática, significa que o lojista pode comercializar os seus produtos por meio de um site exclusivo e personalizado e, se preferir, centralizar ali as suas operações. <br/><br/>Um bom design e um sistema que não vai te deixar na mão, são com certeza os passos mais importantes para se ter uma loja virtual de sucesso. </p> "
+}
